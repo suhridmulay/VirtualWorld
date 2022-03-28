745bd2dc65bd0488d32eef52ce27d78ac179c61e
Suhrid Mulay <suhridmulay@pop-os.localdomain>
2022-03-28 07:49:22 +0530

    Added treasure hunt feature

---
+++ src/treasure.ts
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+
+export class TreasureHuntManager {
+
+    currentSpawnCycle: number;
+    currentSpawnCoordinates: THREE.Vector3;
+
+    currentSpawnModel: THREE.Object3D;
+
+    constructor() {
+        this.currentSpawnCycle = 0;
+        this.currentSpawnCoordinates = new THREE.Vector3();
+        this.currentSpawnModel = new THREE.Object3D();
+    }
+
+    spawnTreasure(scene: THREE.Scene, model: THREE.Object3D, position: THREE.Vector3) {
+        const spawnCoordinates = new THREE.Vector3();
+        spawnCoordinates.copy(position);
+
+        scene.add(model);
+        model.position.copy(spawnCoordinates);
+
+        this.currentSpawnModel = model;
+        this.currentSpawnCycle += 1;
+        this.currentSpawnCoordinates.copy(spawnCoordinates);
+
+        return spawnCoordinates;
+    }
+
+    findTreasure() {
+        const foundingMessage = this.createTreasureFoundMessage((new Date()));
+        return foundingMessage;   
+    }
+
+    createTreasureFoundMessage(time: Date) {
+        const canvas = document.createElement('canvas');
+        canvas.width = window.innerWidth * 5/7;
+        canvas.height = window.innerHeight * 5/7;
+        // Get the drawing context
+        const context = canvas.getContext('2d')!;
+        // Fill it with dark gray fro the background
+        context.fillStyle = "hsla(0, 0%, 20%, 1.0)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        // Create the title
+        const gradient = context.createLinearGradient(0, 0, canvas.width, canvas.height);
+        gradient.addColorStop(0.0, "red");
+        gradient.addColorStop(0.5, "purple");
+        gradient.addColorStop(1.0, "red");
+        context.fillStyle = gradient;
+        context.font = "24px Montserrat";
+        const textsize = context.measureText(`Treasure Hunt #${this.currentSpawnCycle}`);
+        context.fillText(`Treasure Hunt #${this.currentSpawnCycle}`, textsize.width, 30);
+        // Create and write message Body
+        context.fillStyle = "white";
+        context.font = "15px sans-serif";
+        const text = `Congratulation!, you found the treasure at ${time.getMilliseconds()} past the ${time.getSeconds()} second of the ${time.getMinutes()} minute of the ${time.getHours()} hour. That is ${time.toUTCString()} in UTC. Send this to @aarohiworld on Instagram for a chance to win fabulous prizes.`
+        const LINEMAX = canvas.width * 0.9;
+        let lineNo = 5;
+        let lineHeight = 15;
+        let line = '';
+        let x = 0;
+        for (let word of text.split(' ')) {
+            const testLine = line + word + ' ';
+            const testLineSize = context.measureText(testLine);
+            const testWidth = testLineSize.width;
+            if (testWidth > LINEMAX) {
+                context.fillText(line, 0, lineNo * lineHeight)
+                line = word + ' '
+                lineNo += 1;
+            } else {
+                line = testLine;
+            }
+        }
+        context.fillText(line, 0, lineNo * lineHeight);
+        return canvas;
+    }
+}
