2eacf68ed020a2fb4e5183477d10774802e9eeca
Suhrid Mulay <suhridmulay@Suhrids-MacBook-Pro.local>
2022-03-29 13:57:02 +0530

    Added collision with walls

--- src/main.ts
+++ src/main.ts
@@ -79,6 +79,21 @@ function taxicabDistance(v1: THREE.Vector3, v2: THREE.Vector3){
   return d
 }
 
+function inBetween(x: number, a: number, b: number): boolean {
+  return (x > Math.min(a, b)) && (x < Math.max(a, b))
+}
+
+function insideQuad(currentPosition: THREE.Vector2, point1: THREE.Vector2, point2: THREE.Vector2): boolean {
+  return inBetween(currentPosition.x, point1.x, point2.x) && inBetween(currentPosition.y, point1.y, point2.y)
+}
+
+const validArea1Corners = [new THREE.Vector2(-20, -80), new THREE.Vector2(20, 40)]
+const validArea2Corners = [new THREE.Vector2(-60, -40), new THREE.Vector2(60, 140)]
+
+function isPlayerPositionValid(currentPlanePosition: THREE.Vector2): boolean {
+  return insideQuad(currentPlanePosition, validArea1Corners[0], validArea1Corners[1]) 
+    || insideQuad(currentPlanePosition, validArea2Corners[0], validArea2Corners[1])
+}
 
 // Initialise an effect composer
 const composer = new EffectComposer(renderer);
@@ -511,10 +526,6 @@ function gameUpdate() {
 
   const up = new THREE.Vector3(0, 1, 0);
 
-  if (PLAYER.model.position.length() > 100) {
-    PLAYER.model.position.set(0, 0, -80)
-  }
-
   // Rotate hobo model
   hobo.rotateY(2 * Math.PI / 240);
 
@@ -631,7 +642,12 @@ function gameUpdate() {
 
 function update() {
   if (GameState.PlayerState != "INTERACTING") {
+    const oldPlayerPosition = PLAYER.model.position.clone()
     PLAYER.update(worldClock.getDelta());
+    const playerPlanePosition = new THREE.Vector2(PLAYER.model.position.x, PLAYER.model.position.z)
+    if (!isPlayerPositionValid(playerPlanePosition)) {
+      PLAYER.model.position.copy(oldPlayerPosition);
+    }
   }
   gameUpdate();
 }
@@ -649,7 +665,7 @@ window.addEventListener('resize', () => {
 })
 
 // contentLoaded = true;
-PLAYER.model.position.set(0, 0, -80)
+PLAYER.model.position.set(0, 0, -75)
 PLAYER.model.rotateY(Math.PI)
 preloader.style.display = "none";
 
